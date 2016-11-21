384690276070af624ccac54512c20cd487ed3629
joarkosberg <joar_kosberg@hotmail.com>
2016-11-21 14:40:23 +0000

    Read folder and gets files

---
+++ artMerger.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+int main(){
+
+	//Take input (folder with files)
+	printf("Welcome to art merger.\n");
+	printf("Please provide a folder:\n");
+
+	char *folderName = malloc(30*sizeof(char));
+	scanf("%s", folderName);
+
+	printf("Folder to merge: %s\n\n", folderName);
+
+	//Find files
+	char **files;
+	int filesSize = 0;
+	int filesCapacity = 20;
+	files = malloc(filesCapacity*sizeof(char*));
+	for (int i = 0; i < filesCapacity; i++)
+	    files[i] = malloc(30*sizeof(char));
+
+	DIR *d;
+	struct dirent *dir;
+  	d = opendir(folderName);
+  	printf("Files:\n");
+  	if (d){
+    	while ((dir = readdir(d)) != NULL){
+    		char *dot = strrchr(dir->d_name, '.');
+			if (dot && !strcmp(dot, ".txt")){
+				printf("%s\n", dir->d_name);
+				files[filesSize++] = dir->d_name;
+			}
+    	}
+    	closedir(d);
+    }
+    printf("Number of files in folder: %d\n", filesSize);
+
+
+	//Determine Height and Width 
+
+	//Read file after file into new file. 
+
+
+
+	//
+
+
+	return 0;
+}
